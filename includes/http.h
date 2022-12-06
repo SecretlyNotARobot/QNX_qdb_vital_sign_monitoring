d192958107124b5b50d9cd73a32baddd7503a3b6
Kevin <mcclk9052@gmail.com>
2022-12-06 01:09:49 -0500

    Add POST support for greater reliability on networks with poor configuration

--- includes/http.h
+++ includes/http.h
@@ -421,7 +421,7 @@ http_t* http_post( char const* url, void const* data, size_t size, void* memctx 
     internal->socket = socket;
 
     char* request_header;   
-    size_t request_header_len = 64 + strlen( resource ) + strlen( address ) + strlen( port );
+    size_t request_header_len = 100 + strlen( resource ) + strlen( address ) + strlen( port );
     if( request_header_len < sizeof( internal->request_header ) )
         {
         internal->request_header_large = NULL;
@@ -433,7 +433,7 @@ http_t* http_post( char const* url, void const* data, size_t size, void* memctx 
         request_header = internal->request_header_large;
         }       
     int default_http_port = (strcmp(port, "80") == 0);
-    sprintf( request_header, "POST %s HTTP/1.0\r\nHost: %s%s%s\r\nContent-Length: %d\r\n\r\n", resource, address, default_http_port ? "" : ":", default_http_port ? "" : port, 
+    sprintf( request_header, "POST %s HTTP/1.0\r\nHost: %s%s%s\r\nContent-Length: %d\r\nContent-Type: text/plain\r\n\r\n", resource, address, default_http_port ? "" : ":", default_http_port ? "" : port,
         (int) size );
     
     internal->request_data_size = size;
--- remote_test.c
+++ remote_test.c
@@ -1,6 +1,6 @@
 
 #define HTTP_IMPLEMENTATION
-#define CHEAPDB_SERVER_URL "http://cheapdb.azurewebsites.net/db/"
+#define CHEAPDB_SERVER_URL "http://cheapdb.azurewebsites.net/db/post"
 #define DEBUG
 
 #include <stdio.h>
@@ -13,11 +13,11 @@
 #include "includes/http.h"
 
 // sends query, returns the HTTP code on a successful send or -1 on an error
-int sendQuery(char *query, char *response, int responseSize){
-	char requestUrl[responseSize+sizeof(CHEAPDB_SERVER_URL)];
-	sprintf(requestUrl, "%s%s", CHEAPDB_SERVER_URL, query);
-	printf("%s\n", requestUrl);
-	http_t* request = http_get(requestUrl, NULL);
+int sendQuery(char *query, int querySize, char *response, int responseSize){
+	//char requestBody[512];
+	//sprintf(requestBody, "{\"query\":\"%s\"}", query);
+	//printf("%s\n", requestBody);
+	http_t* request = http_post(CHEAPDB_SERVER_URL, query, querySize, NULL);
 
 		if( !request )
 			{
@@ -64,28 +64,28 @@ int sendQuery(char *query, char *response, int responseSize){
 }
 
 int createTable(time_t *date){
-	char tableId[9];
+	char tableId[10];
 	struct tm currentDate;
 	gmtime_r(date, &currentDate);
 
-	sprintf(tableId, "%.2i%.2i%.4i", currentDate.tm_mday, currentDate.tm_mon+1, currentDate.tm_year+1900);
+	sprintf(tableId, "t%.2i%.2i%.4i", currentDate.tm_mday, currentDate.tm_mon+1, currentDate.tm_year+1900);
 	char tableExistsQuery[255];
 	// SELECT object_id FROM sys.tables WHERE name ='{tableId}'
-	sprintf(tableExistsQuery, "SELECT%%20object_id%%20FROM%%20sys.tables%%20WHERE%%20name='%s'", tableId);
+	sprintf(tableExistsQuery, "SELECT object_id FROM sys.tables WHERE name='%s'", tableId);
 
 	printf("%s\n", tableExistsQuery);
 
 	char buf[256];
 
-	if(sendQuery(tableExistsQuery, buf, sizeof(buf)) != 200)
+	if(sendQuery(tableExistsQuery, strlen(tableExistsQuery), buf, sizeof(buf)) != 200)
 		return -1;
 
 	if(strcmp("[]", buf) == 0){
 		char createTableQuery[255];
 		//CREATE TABLE {tableId} (PatientId varchar(16), Heartrate float(4), Temp float(4), Respiration float(4), Sao2 float(4), BloodPressureS float(4))
-		sprintf(createTableQuery, "CREATE%%20TABLE%%20%s%%20(PatientId%%20varchar(16),%%20Heartrate%%20float(4),%%20Temp%%20float(4),%%20Respiration%%20float(4),%%20Sao2%%20float(4),%%20BloodPressureS%%20float(4));", tableId);
+		sprintf(createTableQuery, "CREATE TABLE %s (PatientId varchar(16), Heartrate float(4), Temp float(4), Respiration float(4), Sao2 float(4), BloodPressureS float(4));", tableId);
 
-		printf("rec code %i, %s\n", sendQuery(createTableQuery, buf, sizeof(buf)), buf);
+		printf("rec code %i, %s\n", sendQuery(createTableQuery, strlen(createTableQuery), buf, sizeof(buf)), buf);
 	}
 
 
